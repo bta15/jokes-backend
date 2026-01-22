470331d2f8228b6c2b70f250e230ae4cecfa3354
bta15 <besha.taher74@gmail.com>
2026-01-22 13:55:38 +0100

    add service test

--- src/main/java/jokes/jokes/service/JokeService.java
+++ src/main/java/jokes/jokes/service/JokeService.java
@@ -3,16 +3,18 @@ package jokes.jokes.service;
 import jokes.jokes.controller.dto.JokeDto;
 import jokes.jokes.database.JokeRepository;
 import jokes.jokes.database.entity.JokeEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class JokeService {
 
-    @Autowired
-    private JokeRepository jokeRepository;
+    private final JokeRepository jokeRepository;
 
     public JokeEntity getJoke(Long id) {
         var joke = jokeRepository.findById(id);
---
+++ src/test/java/jokes/jokes/service/JokeServiceTest.java
@@ -0,0 +1,46 @@
+package jokes.jokes.service;
+
+import jokes.jokes.database.JokeRepository;
+import jokes.jokes.database.entity.JokeEntity;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class JokeServiceTest {
+
+
+    private JokeService jokeService;
+
+    @Mock
+    private JokeRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        jokeService = new JokeService(repository);
+    }
+
+    @AfterEach
+    void tearDown() {
+        verifyNoMoreInteractions(repository);
+    }
+
+    @Test
+    void like() {
+        when(repository.findById(anyLong())).thenReturn(Optional.of(new JokeEntity()));
+
+        jokeService.like(1L);
+
+        verify(repository).findById(anyLong());
+        verify(repository).save(any(JokeEntity.class));
+    }
+
+}
